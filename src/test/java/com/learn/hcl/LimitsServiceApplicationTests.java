d6ca227b8a6c2f49c4279f14d1e3e7596bf226b5
manish436 <manish436@gmail.com>
2020-02-03 21:43:01 +0530

    Initial commit

---
+++ src/test/java/com/learn/hcl/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.hcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
